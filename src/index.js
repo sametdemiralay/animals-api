bbd37c99ccd1fecf1c8791ec8796a5126025a113
sametdemiralay <demiralayahmetsamet@gmail.com>
2025-02-08 18:41:46 +0300

    added statickData get request

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import { animals } from "./staticData/index.js";
 
 const app = express();
 
@@ -6,6 +7,39 @@ const PORT = process.env.PORT || 3000;
 
 app.get("/", (request, response) => {
   response.status(201).send({ msg: "Hi samco!" });
+});
+
+// Hepsini yada Filtrelenmiş Halini Getirme (Query Parameters)
+// .../api/animals
+// .../api/animals?filter=name&value=in
+app.get("/api/animals", (request, response) => {
+  console.log(request.query);
+  const {
+    query: { filter, value },
+  } = request;
+
+  if (filter && value) {
+    return response.send(
+      animals.filter((animal) => animal[filter].includes(value))
+    );
+  }
+
+  return response.send(animals);
+});
+
+// Tek Birini Getirme id ye göre (Route Parameters)
+// .../api/animals/3
+app.get("/api/animals/:id", (request, response) => {
+  console.log(request.params);
+  const parsedId = parseInt(request.params.id);
+
+  if (isNaN(parsedId)) {
+    return response.status(400).send({ msg: "Bad Request. Invalid ID." });
+  }
+
+  const findUser = animals.find((animal) => animal.id === parsedId);
+  if (!findUser) return response.sendStatus(404);
+  return response.send(findUser);
 });
 
 app.listen(PORT, () => {
---
+++ src/staticData/index.js
@@ -0,0 +1,12 @@
+export const animals = [
+  { id: 1, name: "Lion", type: "Wild", age: 5 },
+  { id: 2, name: "Penguin", type: "Bird", age: 3 },
+  { id: 3, name: "Elephant", type: "Wild", age: 10 },
+  { id: 4, name: "Dog", type: "Domestic", age: 2 },
+  { id: 5, name: "Cat", type: "Domestic", age: 4 },
+  { id: 6, name: "Dolphin", type: "Sea", age: 8 },
+  { id: 7, name: "Parrot", type: "Bird", age: 1 },
+  { id: 8, name: "Snake", type: "Reptile", age: 3 },
+  { id: 9, name: "Rabbit", type: "Domestic", age: 2 },
+  { id: 10, name: "Fish", type: "Sea", age: 1 },
+];
